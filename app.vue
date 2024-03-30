88f281d78b4df28602641b7c4b3a65aa916de758
Tiago Magalhães <tmagdev@gmail.com>
2024-03-30 19:19:04 +0000

    More work on the cards so they display an image.

--- app.vue
+++ app.vue
@@ -2,10 +2,28 @@
     <div class="app">
         <Header/>
         <div class="content">
-            <Card>Example</Card>
-            <Card>Example</Card>
-            <Card>Example</Card>
-            <Card>Example</Card>
+            <Card v-for="(item) in items" :image="item.image" :title="item.title"/>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+const items = ref([
+    {
+        image: "/images/sdt-logo.png",
+        title: "Sonic Dream Team"
+    },
+    {
+        image: "/images/sdt-logo.png",
+        title: "Alloy"
+    },
+    {
+        image: "/images/sdt-logo.png",
+        title: "iCliGo"
+    },
+    {
+        image: "/images/sdt-logo.png",
+        title: "Bitclubs"
+    }
+]);
+</script>
--- components/Card.vue
+++ components/Card.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="card">
-        <slot/>
+        <img class="card-image" :src="image" :alt="title"/>
     </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+    image: string,
+    title: string,
+    text?: string
+}>()
+</script>
