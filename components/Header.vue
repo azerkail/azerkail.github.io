44ba7f1e21cf9e729e330d7e0a696c1f7021481c
Tiago Magalhães <tmagdev@gmail.com>
2024-03-30 18:09:19 +0000

    Making everything stand out from the background a bit.

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,23 @@
     <header>
         <h1>Tiago Magalhães</h1>
         <div class="divider"/>
-        <font-awesome-icon icon="fa-brands fa-twitter"/>
+        <ul class="social">
+            <li>
+                <a href="https://twitter.com/azerkail" target="_blank">
+                    <font-awesome-icon class="icon" icon="fa-brands fa-twitter"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/azerkail" target="_blank">
+                    <font-awesome-icon class="icon" icon="fa-brands fa-github"/>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/tmagdev/" target="_blank">
+                    <font-awesome-icon class="icon" icon="fa-brands fa-linkedin"/>
+                </a>
+            </li>
+        </ul>
     </header>
 </template>
 
