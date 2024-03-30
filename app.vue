1d48351532e37cd1f22779ffd21ab48e900c3591
Tiago Magalhães <tmagdev@gmail.com>
2024-03-30 19:01:45 +0000

    Finished styling the cards.

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="app">
         <Header/>
+        <div class="content">
+            <Card>Example</Card>
+            <Card>Example</Card>
+            <Card>Example</Card>
+            <Card>Example</Card>
+        </div>
     </div>
 </template>
