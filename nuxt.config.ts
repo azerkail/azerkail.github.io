56451b3a8408b466664edbcf6a0d1b0028a0b7e8
Tiago Magalhães <tmagdev@gmail.com>
2024-03-30 12:15:22 +0000

    Creating some base files to start the website with.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  typescript: {
-    typeCheck: true
-  }
+    devtools: {enabled: true},
+    typescript: {
+        typeCheck: true
+    },
+    ssr: true,
+    spaLoadingTemplate: false
 })
