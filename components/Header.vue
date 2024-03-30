70e68e23c1f77b8ed06c2c68f09598699a8441b3
Tiago Magalhães <tmagdev@gmail.com>
2024-03-30 17:45:23 +0000

    Finally got brands working.

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,10 @@
     <header>
         <h1>Tiago Magalhães</h1>
         <div class="divider"/>
-        <Icon/>
+        <font-awesome-icon icon="fa-brands fa-twitter"/>
     </header>
 </template>
+
+<script setup lang="ts">
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+</script>
